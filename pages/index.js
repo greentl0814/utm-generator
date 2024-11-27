049db183691df4f29d8c161c0043cf653371a9f4
greentl0814 <kj0815@gmail.com>
2024-11-27 18:11:41 +0900

    ga install fix

--- pages/index.js
+++ pages/index.js
@@ -154,9 +154,9 @@ export default function UTMGenerator() {
   return (
     <>
       <Head>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-65TMBKVV6G"></script>
-        <script>
+       <!-- Google tag (gtag.js) -->
+       <script async src="https://www.googletagmanager.com/gtag/js?id=G-65TMBKVV6G"></script>
+       <script>
          window.dataLayer = window.dataLayer || [];
          function gtag(){dataLayer.push(arguments);}
          gtag('js', new Date());
