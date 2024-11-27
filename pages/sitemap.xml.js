4f9f490003602d7f4c7b4be0c69dcb63e5ae7fb8
greentl0814 <kj0815@gmail.com>
2024-11-27 17:57:07 +0900

    Add sitemap.xml

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,32 @@
+const EXTERNAL_DATA_URL = 'https://utm-generator-zeta.vercel.app';
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>${EXTERNAL_DATA_URL}</loc>
+       <lastmod>${new Date().toISOString()}</lastmod>
+       <changefreq>daily</changefreq>
+       <priority>1.0</priority>
+     </url>
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  const sitemap = generateSiteMap();
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
