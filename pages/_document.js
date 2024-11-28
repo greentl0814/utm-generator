106ea67e39ef64d0b25571a8cef5b55ccca51de3
greentl0814 <kj0815@gmail.com>
2024-11-28 16:05:08 +0900

    adsense

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+ return (
+   <Html lang="en">
+     <Head>
+       <script
+         async
+         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6071061687711848"
+         crossOrigin="anonymous"
+       />
+     </Head>
+     <body className="antialiased">
+       <Main />
+       <NextScript />
+     </body>
+   </Html>
+ );
 }
