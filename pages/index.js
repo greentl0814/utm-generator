d677919f016a182512b17ea8e2147f1ac0dcd488
greentl0814 <kj0815@gmail.com>
2024-12-02 07:29:42 +0900

    Add redirect page to new marketing tools-2

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Redirect() {
               {countdown}초 후 자동으로 이동됩니다
             </div>
           </div>
-
+          <a
             href={newUrl}
             className="inline-block bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600 transition-colors"
           >
